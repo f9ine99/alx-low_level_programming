9ebac15d7e9d6a9e349d1795f365c22607e6d490
kiya <f9ine99@gmail.com>
2022-12-08 02:31:57 -0500

    working with c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-puts("\"programming is like bulding a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
     return (0);
 }
