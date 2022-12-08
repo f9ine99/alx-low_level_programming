a4fa76cdca793b3e0bcc1c226f0ca5886234c776
kiya <f9ine99@gmail.com>
2022-12-08 03:09:17 -0500

    working with c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry Point
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-                    puts("programming is like building a multilingual puzzle\n");
+                    puts("\"programming is like building a multilingual puzzle");
                                                      return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
+*main - Entry Point
+*
 *Return: Always 0 (Success)
 */
 
