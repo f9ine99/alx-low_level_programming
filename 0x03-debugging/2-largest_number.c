baa80d400b853341e5b12fa44a3eb70cbc24ab35
kiya <f9ine99@gmail.com>
2022-12-15 06:32:35 -0500

    working with c programming

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#include "fira.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "fira.h"
+#include "main.h"
 
 /**
  * main - prints the largest of 3 integers
